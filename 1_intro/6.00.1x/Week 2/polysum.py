cdddcebc314c3e1ab31bd6013818023dbd0bf26c
emyarod <allllaboutyou@gmail.com>
2016-06-16 16:21:05 -0700

    Complete polysum function

--- 1_intro/6.00.1x/Week 2/polysum.py
+++ 1_intro/6.00.1x/Week 2/polysum.py
@@ -4,4 +4,12 @@
 
 # The perimeter of a polygon is: length of the boundary of the polygon
 
-# Write a function called `polysum` that takes 2 arguments, `n` and `s`. This function should sum the area and square of the perimeter of the regular polygon. The function returns the rum, rounded to 4 decimal places.
+# Write a function called `polysum` that takes 2 arguments, `n` and `s`. This function should sum the area and square of the perimeter of the regular polygon. The function returns the sum, rounded to 4 decimal places.
+
+import math
+
+def polysum(n, s):
+    area = (0.25 * n * s**2) / (math.tan(math.pi / n))
+    perimeter = n * s
+    polysum = area + perimeter**2
+    return round(polysum, 4)
