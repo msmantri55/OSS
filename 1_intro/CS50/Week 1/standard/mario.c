b4f1a2eb6d08e49451f3e4e2cbd2bad286e43115
emyarod <allllaboutyou@gmail.com>
2016-06-24 02:28:18 -0700

    Separate hacker and standard pset solutions

---
+++ 1_intro/CS50/Week 1/standard/mario.c
@@ -0,0 +1,57 @@
+/**
+ * program that recreates Mario half-pyramids using hashes (#) for blocks.
+ * First prompt the user for the half-pyramids' heights, a non-negative integer
+ * no greater than 23.
+ * Then, generate (with the help of printf and one or more loops) the desired
+ * half-pyramids. Take care to left-align the bottom-left corner of the
+ * left-hand half-pyramid
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+// get pyramid height from user
+int getPyramidHeight(void) {
+  int height = 0;
+
+  do {
+    printf("Pyramid height (must be < 24): ");
+    height = GetInt();
+  } while(height > 23 || height < 0);
+
+  return height;
+}
+
+
+int addSpaces(numSpaces) {
+  for (int i = 0; i < numSpaces; i++) {
+    printf(" ");
+  }
+
+  return 0;
+}
+
+int addHashes(numHashes) {
+  for (int i = 0; i < numHashes; i++) {
+    printf("#");
+  }
+
+  return 0;
+}
+
+int main(void) {
+  int height = getPyramidHeight();
+  int spaces = height - 1;
+
+  for (int i = 1; i <= height; i++) {
+    // first half
+    addSpaces(spaces);
+    addHashes(i);
+    printf("\n");
+
+    // decrease indentation
+    spaces--;
+  }
+
+  return 0;
+}
