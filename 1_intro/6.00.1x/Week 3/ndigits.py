a20f2a4c05cb7e10deacdce83c1d115c7523de51
emyarod <allllaboutyou@gmail.com>
2016-06-24 00:11:23 -0700

    Add integer digit counter

---
+++ 1_intro/6.00.1x/Week 3/ndigits.py
@@ -0,0 +1,16 @@
+# Write a function called ndigits, that takes an integer x (either positive or negative) as an
+# argument. This function should return the number of digits in x.
+
+def ndigits(integer):
+    # Ensure we properly count the number of digits
+    integer = abs(integer)
+
+    # base case is 1 since the minimum number of digits has to be 1
+    counter = 1
+    
+    if integer == 0:
+        return 0
+    else:
+        return counter + ndigits(integer / 10)
+
+print ndigits(-9090428)
